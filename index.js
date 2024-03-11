dac1e1dfcb45b3601b5ff5715571806b9ad7be2d
kamal0620040 <neupane0620040@outlook.com>
2024-03-11 15:04:11 +0545

    Implemented proxy from APIGateway to bookingservice

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
 const PORT = 3004;
 
+app.use(morgan("combined"));
+app.use(
+  "/bookingservice",
+  createProxyMiddleware({ target: "http://localhost:3002", changeOrigin: true })
+);
 app.get("/home", (req, res) => {
   return res.json({ message: "OK" });
 });
