836a5773eaaf8e19906cf10a6e95e0deda898c42
kamal0620040 <neupane0620040@outlook.com>
2024-03-11 15:22:10 +0545

    RateLimiter using express-rate-limit

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
+import { rateLimit } from "express-rate-limit";
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
 const PORT = 3004;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100,
+});
+
 app.use(morgan("combined"));
+app.use(limiter);
 app.use(
   "/bookingservice",
   createProxyMiddleware({ target: "http://localhost:3002", changeOrigin: true })
