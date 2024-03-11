20187329b01effbd52e0eae9a8df91f54a0f94f0
kamal0620040 <neupane0620040@outlook.com>
2024-03-11 15:41:25 +0545

    Wrapped bookingservice inside the authorization

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+import axios from "axios";
 import { rateLimit } from "express-rate-limit";
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
@@ -14,6 +15,32 @@ const limiter = rateLimit({
 
 app.use(morgan("combined"));
 app.use(limiter);
+
+app.use("/bookingservice", async (req, res, next) => {
+  console.log(req.headers["x-access-token"]);
+  try {
+    const response = await axios.get(
+      "http://localhost:3001/api/v1/isauthenticated",
+      {
+        headers: {
+          "x-access-token": req.headers["x-access-token"],
+        },
+      }
+    );
+    if (response.data.success) {
+      next();
+    } else {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+});
+
 app.use(
   "/bookingservice",
   createProxyMiddleware({ target: "http://localhost:3002", changeOrigin: true })
